390af2aec1efd70c5f234a70feae736f676e1ac4
Lucas Carvalho <lucasvierac@gmail.com>
2025-02-13 11:17:58 -0300

    feat: add utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const normalizeText = (text: string) =>
+  text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
