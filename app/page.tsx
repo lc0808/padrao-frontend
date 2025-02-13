b33f002f89905fcc7f4a57378b519cc77d6c2310
Lucas Carvalho <lucasvierac@gmail.com>
2025-02-13 11:21:07 -0300

    feat: add home page with product listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
+import ProductFilter from "../components/ProductFilter";
+import ProductCard from "../components/ProductCard";
+import { useProductContext } from "../contexts/ProductContext";
+import Loading from "../components/Loading";
+
 export default function Home() {
+  const { filteredProducts, isLoading, error } = useProductContext();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        teste
-      </main>
+    <div className="flex flex-col font-montserrat ">
+      <div
+        className={`bg-[url('../public/assets/services.webp')] bg-cover bg-top bg-no-repeat aspect-[29/8.5] `}
+      />
+      <ProductFilter />
+
+      {isLoading && <Loading />}
+      {error && <p>Error: {error.message}</p>}
+
+      {!isLoading && (
+        <>
+          <div className="flex justify-between px-10">
+            <p className="text-lg">{filteredProducts.length} Produtos</p>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 px-10 py-14 ">
+            {filteredProducts.map((product) => (
+              <div key={product.id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
