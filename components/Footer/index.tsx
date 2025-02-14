88bc1c898e608ed515e176187bf8cdd273e29929
Lucas Carvalho <lucasvierac@gmail.com>
2025-02-14 18:44:18 -0300

    fix: Logo path

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <div className="font-montserrat flex flex-row justify-between w-full items-center p-2 sm:p-6 pb-0 sm:pb-1 bg-[#f2f2f2]">
       <Image
-        src="assets/logo.svg"
+        src="/assets/logo.svg"
         alt="ECommerce Logo"
         width={300}
         height={300}
@@ -22,28 +22,28 @@ export default function Footer() {
         <Button
           variant={"ghost"}
           onClick={() => setActiveFilter(ProductCategory.All)}
-          className="hover:font-medium transition-all ease-in-out duration-150 text-xs sm:text-base max-sm:px-0"
+          className="hover:font-medium transition-all ease-in-out duration-150 text-xs md:text-base max-md:px-0"
         >
           Home
         </Button>
         <Button
           variant={"ghost"}
           onClick={() => setActiveFilter(ProductCategory.Sneakers)}
-          className="hover:font-medium transition-all ease-in-out duration-150 text-xs sm:text-base max-sm:px-0"
+          className="hover:font-medium transition-all ease-in-out duration-150 text-xs md:text-base max-md:px-0"
         >
           Tênis
         </Button>
         <Button
           variant={"ghost"}
           onClick={() => setActiveFilter(ProductCategory.TShirts)}
-          className="hover:font-medium transition-all ease-in-out duration-150 text-xs sm:text-base max-sm:px-0"
+          className="hover:font-medium transition-all ease-in-out duration-150 text-xs md:text-base max-md:px-0"
         >
           Camisetas
         </Button>
         <Button
           variant={"ghost"}
           onClick={() => setActiveFilter(ProductCategory.Pants)}
-          className="hover:font-medium transition-all ease-in-out duration-150 text-xs sm:text-base max-sm:px-0.5"
+          className="hover:font-medium transition-all ease-in-out duration-150 text-xs md:text-base max-md:px-0.5"
         >
           Calças
         </Button>
